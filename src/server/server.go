568654cd0f68b9dbb48def79c224b9ea5195c6cd
thkim <zjaekrxj7@gmail.com>
2019-01-21 04:48:02 -0500

    init

--- src/server/server.go
+++ src/server/server.go
@@ -1,33 +1,61 @@
 package server
 
 import (
+    "io/ioutil"
+    "net/http"
+    "path/filepath"
+	"log"
 	"fmt"
-fmt.Fprintf(w, "Go Server Working!")	"log"
-	"net/http"
-	"strings"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-
-	fmt.Println("default: ", r.Form)
-	fmt.Println("path: ", r.URL.Path)
-	fmt.Println("param: ", r.Form["test_param"])
-
-	for k, v := range r.Form {
-		fmt.Println("key: ", k)
-		fmt.Println("val: ", strings.Join(v, ""))
-	}
-
-	fmt.Fprintf(w, "Go Server Working!")
-}
-
 func Start() {
-	http.HandleFunc("/", Handler)
-	err := http.ListenAndServe(":8080", nil)
+    http.Handle("/", new(staticHandler))
+
+    err := http.ListenAndServe(":8080", nil)
+
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	} else {
 		fmt.Println("ListenAndServe Started! -> Port(8080)")
 	}
+}
+
+type staticHandler struct {
+    http.Handler
+}
+
+func (h *staticHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+    localPath := "html" + req.URL.Path
+    content, err := ioutil.ReadFile(localPath)
+    if err != nil {
+        w.WriteHeader(404)
+        w.Write([]byte(http.StatusText(404)))
+        return
+    }
+
+    contentType := getContentType(localPath)
+    w.Header().Add("Content-Type", contentType)
+    w.Write(content)
+}
+
+func getContentType(localPath string) string {
+    var contentType string
+    ext := filepath.Ext(localPath)
+
+    switch ext {
+    case ".html":
+        contentType = "text/html"
+    case ".css":
+        contentType = "text/css"
+    case ".js":
+        contentType = "application/javascript"
+    case ".png":
+        contentType = "image/png"
+    case ".jpg":
+        contentType = "image/jpeg"
+    default:
+        contentType = "text/plain"
+    }
+
+    return contentType
 }
